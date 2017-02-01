70f016e33f82c109dd0e78be3237164de88db463
GlaDos <glados28@yandex.ru>
2017-02-01 17:53:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/****************************
+ * Launcher module.
+ *
+ * @author GlaDos
+ * @since 01.02.17
+ ****************************/
+
+"use strict";
+
+/**
+ * Imports.
+ */
+const _ = require("");
+
+/**
+ * Constants.
+ */
+const __ = _;
+
+
+
+/**
+ * Exports.
+ */
+exports = module.exports = {};
